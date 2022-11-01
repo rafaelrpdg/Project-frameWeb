8a65968c78f9d3592f1ede57a95500e6212401aa
Rafael Rodrigues <raphapereira2011@gmail.com>
2022-10-31 23:06:08 -0300

    adicionando databinding

--- projectfirst/src/app/data-dinbind/data-dinbind.component.ts
+++ projectfirst/src/app/data-dinbind/data-dinbind.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-dinbind',
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataDinbindComponent implements OnInit {
 
-  url: any = 'http://www.google.com'; 
-  constructor() { }
+  url: any = "https://www.youtube.com/playlist?list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G"; 
+  nomeC : string = "Meu nome Completo é RAfael Rdorgieus pereria da graça";
+  imagem = 'https://picsum.photos/200/300';
+  constructor() { 
+
+
+  }
 
   ngOnInit(): void {
   }
